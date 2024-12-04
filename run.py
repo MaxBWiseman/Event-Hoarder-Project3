f2d65814aac78edc5345f1386af3ef24dfb682d2
Max Wiseman <maxwise70@hotmail.co.uk>
2024-12-04 16:31:08 +0000

    refactor: fix line length in menu

--- run.py
+++ run.py
@@ -1928,10 +1928,9 @@ def main():
               ' also print to Excel or CSV'
               '\nSearch for events with either of the first 3 options,'
               '\nAfter viewing your searched events, you can view them'
-              ' in the database\nwith option 4 or quickly access within searches with "T"'
-              '\nOption 5 is for viewing links to saved Excel,'
-              ' CSV or data visuals that\n'
-              'you may make.'
+              ' in the database\nwith option 4 or quickly access within'
+              ' searches with "T" \nOption 5 is for viewing links to saved Excel,'
+              ' CSV or data visuals that\nyou may make.'
               '\n-------------------------------------')
         print('\nChoose an option:')
         print('1. Quick Search & Collect')
